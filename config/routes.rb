ddb92eebc195a8748b40ccbe6a4d701b548a814b
Ariane Deschenes Foucher <ariane.df@me.com>
2016-04-12 16:25:03 -0400

    Creating a user model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  root 'products#index'
+root 'products#index'
 resources :products
+resources :users, only: [:new, :create]
+
+# get 'users/new'
+#
+# get 'users/create'
+#
+# root 'products#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
---
+++ db/migrate/20160412201450_rename_password_digest_in_users.rb
@@ -0,0 +1,5 @@
+class RenamePasswordDigestInUsers < ActiveRecord::Migration
+  def change
+    rename_column :users, :password_digets, :password_digest
+  end
+end
